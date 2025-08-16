e352be8eae3e3af3593d09942705ad80e0eec7ba
AAAA <AAAA@AAAA.com>
2025-08-16 13:25:03 +0900

    feat: add calc and sum logic

---
+++ calc.js
@@ -0,0 +1,7 @@
+function sum(...args) {
+  return args.reduce((a, b) => a + b);
+}
+
+function sub(...args) {
+  return args.reduce((a, b) => a - b);
+}
---
+++ sum.js
@@ -0,0 +1,4 @@
+function sum(a, b) {
+  const result = a + b;
+  return result;
+}
